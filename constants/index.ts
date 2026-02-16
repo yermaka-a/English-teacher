3d6251537bdd14367ea5d4a05d92092d51c065bf
yermaka <ermak.080@mail.ru>
2026-02-17 04:07:57 +0800

    add consts

---
+++ constants/index.ts
@@ -0,0 +1,1 @@
+export { TABS_NAMES, TABS_TITLE } from "./tabs";
---
+++ constants/tabs.ts
@@ -0,0 +1,8 @@
+export const TABS_NAMES = {
+  home: "Главная",
+  practice: "Упражнения",
+  library: "Библиотека",
+  create: "Создать",
+} as const;
+
+export const TABS_TITLE = "Учись с удовольствием!";
