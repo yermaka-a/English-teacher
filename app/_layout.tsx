e8275cf22f0378a431b52b8b46054db03d4b408d
yermaka <ermak.080@mail.ru>
2026-02-17 04:06:50 +0800

    add loader to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-
+import { ActivityIndicator, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import "./global.css";
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, LoadingError] = useFonts({
+    LoraMediumItalic: require("@/assets/fonts/Lora/static/Lora-MediumItalic.ttf"),
+    Roboto: require("@/assets/fonts/Roboto/static/Roboto-Black.ttf"),
+  });
+  if (fontsLoaded) {
+    return (
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
+    );
+  } else if (LoadingError) {
+    console.error("loading error: ", LoadingError);
+    return null;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#3b82f6" />
+      </View>
+    );
+  }
 }
